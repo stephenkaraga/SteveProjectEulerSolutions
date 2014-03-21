8ca6010a9eb72d27defc28d9a08b2b240e252d45
stephenkaraga <stephenkaraga@gmail.com>
2014-03-21 01:38:47 -0400

    Finished first problem

---
+++ Problem1.rb
@@ -0,0 +1,23 @@
+=begin
+	
+If we list all 
+the natural numbers 
+below 10 that are multiples 
+of 3 or 5, we get 3, 5, 6 and 9. 
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+	
+=end
+puts "test"
+def solution
+	arr = []
+	1.upto(999) do |num|
+		if num % 3 == 0 || num % 5 == 0
+			arr.push(num)
+		end
+	end
+	 sol = arr.reduce(:+)
+	 sol
+	 puts sol
+end
+solution

dd4723c48633d2d95d0e5b7302f7290f7f755a74
stephenkaraga <stephenkaraga@gmail.com>
2014-03-23 15:01:18 -0400

    Solved Problem16

---
+++ Problem16.rb
@@ -0,0 +1,9 @@
+=begin
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+
+=end
+a = 2**1000
+sum = a.to_s.split("").map! {|digit| digit.to_i}.reduce(:+)
+puts sum
